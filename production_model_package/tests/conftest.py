d34463ac2852e7bc5337d04fb11da11fc031720e
Artur Kim <archi.baldi@bk.ru>
2023-10-23 05:15:35 +0900

    Initial commit

---
+++ production_model_package/tests/conftest.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.append('C:/Users/Irek9/OneDrive/Документы/KV_DW_JL_5/ML_deployments/production_model_package')
+
+import pytest
+
+from regression_model.config.core import config
+from regression_model.processing.data_manager import load_dataset
+
+
+@pytest.fixture()
+def sample_input_data():
+    return load_dataset(file_name=config.app_config.test_data_file)
+
---
+++ titanic_dataset/tests/test_features.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.append("C:\\Users\\Irek9\\OneDrive\\Документы\\KV_DW_JL_5\\ML_deployments\\titanic_dataset\\")
+
+from classification_model.config.core import config
+from classification_model.processing.features import ExtractLetterTransformer
+
+def test_temporal_variable_transformer(sample_input_data):
+
+    #Given
+
+    transformer = ExtractLetterTransformer(
+        variables=config.model_config.cabin_vars
+    )
+
+    assert sample_input_data["cabin"].iat[6] == "E12"
+
+    #When
+    subject = transformer.fit_transformer(sample_input_data)
+
+    #Then
+    assert subject["cabin"].iat[6] == "E"
