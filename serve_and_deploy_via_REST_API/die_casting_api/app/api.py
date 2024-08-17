7eb7a61d7b91b807076a70aaffb1f3bc0591070d
Artur Kim <65280247+Archi68@users.noreply.github.com>
2024-08-17 20:27:25 +0900

    updates

--- serve_and_deploy_via_REST_API/die_casting_api/app/api.py
+++ serve_and_deploy_via_REST_API/die_casting_api/app/api.py
@@ -37,10 +37,10 @@ async def predict(input_data: schemas.MultipleDieCastingDataInputs) -> Any:
 
     logger.info(f"Making prediction on inputs: {input_data.inputs[:2]}")
     results = make_prediction(input_data=input_df.replace({np.nan: None}))
-    print(f"isinstance(results.get('predictions'), list) => {isinstance(results.get('predictions'), list)}")
+
     if not isinstance(results.get('predictions'), list):
+        # Predictions must be a list
         results['predictions'] = list(results.get('predictions'))
-        #raise ValueError("Predictions must be a list")
 
     if results["errors"] is not None:
         logger.warning(f"Prediction validation error: {results.get('errors')}")
